b7b4ef6b2832f706df48a251814bbfadbc5a8bd7
e_kolotilko <E.Kolotilko@gmail.com>
2019-10-13 21:50:45 +0300

    Main functions working. Has bugs, needs hardening

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,4 +1,4 @@
-
+/*
 function main(){
 	var loggerDiv = document.getElementById('logging_div');
 	var request = new XMLHttpRequest();
@@ -9,6 +9,41 @@ function main(){
 		loggerDiv.innerText = request.responseText;
 	};
 	request.send();
+}
+
+main();
+*/
+
+function appendDiv(response){
+	var text = response.body;
+	var loggerDiv = document.getElementById('logging_div');
+	var newElem = document.createElement('div')
+	newElem.innerText = text;
+	loggerDiv.prepend(newElem);
+}
+
+function main(){
+	var ws = new WebSocket('ws://192.168.0.102:15674/ws');
+	ws.onmessage=function(x){
+		console.log('ws got it!');		
+	}
+	
+	var client = Stomp.over(ws);
+	var on_connect = function() {
+		var id = client.subscribe("/exchange/border_cross", function(d) {
+			appendDiv(d);
+		  });
+	    console.log("ID jsonofied:"+JSON.stringify(id))
+	};
+	var on_error =  function() {
+	    console.log('error');
+	};
+	client.onreceive = function(response) {
+		console.log('Got it');
+		appendDiv(response);
+    }
+	
+	client.connect('reader_tester', 'reader', on_connect, on_error, '/');
 }
 
 main();

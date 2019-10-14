4206eccd1f3fb3bb3b3e496b67a608ca2b949406
e_kolotilko <E.Kolotilko@gmail.com>
2019-10-14 14:19:27 +0300

    JS brutal reconnection done

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -14,8 +14,7 @@ function main(){
 main();
 */
 
-function appendDiv(response){
-	var text = response.body;
+function appendDiv(text){
 	var loggerDiv = document.getElementById('logging_div');
 	var newElem = document.createElement('div')
 	newElem.innerText = text;
@@ -29,17 +28,21 @@ function main(){
 	var client = Stomp.over(ws);
 	var on_connect = function() {
 		var id = client.subscribe("/exchange/border_cross", function(d) {
-			appendDiv(d);
+			appendDiv(d.body);
 		  });
 	    console.log("ID jsonofied:"+JSON.stringify(id))
 	};
 	var on_error =  function() {
-	    console.log('error');
+	    //console.log('error');
+		appendDiv('Connection error.....');
+		//client.connect('reader_tester', 'reader', on_connect, on_error, '/');
+		setTimeout(main,30000);
 	};
 	client.onreceive = function(response) {
 		console.log('Got it');
 		appendDiv(response);
     }
+	client.reconnect_delay = 5000;
 	
 	client.connect('reader_tester', 'reader', on_connect, on_error, '/');
 }
